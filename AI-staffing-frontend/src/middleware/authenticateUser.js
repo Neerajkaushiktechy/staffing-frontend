4ec45d8624af29a63bd98a310576e87728080936
Neerajkaushiktechy <neeraj.kaushiktechy@gmail.com>
2025-05-01 18:36:26 +0530

    minor change

--- AI-staffing-frontend/src/middleware/authenticateUser.js
+++ AI-staffing-frontend/src/middleware/authenticateUser.js
@@ -3,7 +3,6 @@ import { token_verify } from "../urls/adminUrls"
 
 const authenticateUser = async () => {
     try {
-        console.log("Verifying token...");
         const res = await get(token_verify, true);
         if (res?.data?.status === 200) {
             return true;
